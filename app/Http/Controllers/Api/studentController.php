f25204ffb2c08fbce59336c7a74afc4fdb5e3850
juanpa1979 <juan.pablo.duarte.1979@gmail.com>
2024-09-22 16:13:39 -0400

    Atualización practica de CRUD en Laravel

--- app/Http/Controllers/Api/studentController.php
+++ app/Http/Controllers/Api/studentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api; // Este comando indica la ruta en la que estoy situado
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
